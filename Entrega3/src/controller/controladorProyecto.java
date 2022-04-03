8bbee2c3b61beb73f55a622b18ea3657aa5794ea
DanielMogu <j.monterog@uniandes.edu.co>
2022-04-02 23:27:11 -0500

    Comentarios

--- Entrega3/src/controller/controladorProyecto.java
+++ Entrega3/src/controller/controladorProyecto.java
@@ -9,20 +9,38 @@ import model.*;
 
 public class controladorProyecto {
 	
+	// La clase "controladorProyecto" posee los siguientes atributos:
+	// proyecto Proy <<una instancia de la clase proyecto que representa el proyecto que se está trabajando>>
+	// HashMap<String, integrante> usuarios <<un HashMap con los usuarios del proyecto>>
+	// actividad ActividadActual <<una instancia de la clase actividad que representa la actividad que se está trabajando>>
+	// registro registroActual <<una instancia de la clase registro que representa el registro que se está trabajando>>
+	
 	private proyecto Proy;
 	private HashMap<String, integrante> usuarios;
 	private actividad ActividadActual;
 	private registro registroActual;
 	
+	// Se ingresa un proyecto, se cargan los usuarios en base a este proyecto y
+	// se establece como la variable "Proy" del controlador.
 	
 	public void agregarProyecto(proyecto Proy) throws FileNotFoundException, IOException {
 		cargarUsuarios();
 		this.Proy= Proy;
 	}
 	
+	// Retorna un String con el nombre del integrante al que se atribuye el registro.
+	
 	public String getCurrentLog() {
 		return registroActual.getAmigoName();
 	}
+	
+	// Se ingresa un String con el comentario de un registro y un integrante "usuario"
+	// y se lleva a cabo el protocolo de acabar una actividad:
+	//
+	// Se crea un registro con el comentario dado, y con los minutos transcurridos
+	// se actualiza el tiempo transcurrido de la actividad actual, se imprime la información
+	// sobre el registro que se creó y se actualiza el archivo de usuarios con la nueva
+	// información recibida.
 	
 	public void acabarActividad(String comentario, integrante usuario) {
         int minutosTranscurridos = Math.abs(registroActual.terminarTurno(comentario));
@@ -36,14 +54,24 @@ public class controladorProyecto {
 		}
     }
 	
+	// Se crea un HashMap con los usuarios del proyecto y se establece en la variable
+	// "usuarios" del proyecto.
+	
 	private void  cargarUsuarios() throws FileNotFoundException, IOException {
 		this.usuarios = loaderProyect.getUserList();
-		
 	}
+	
+	// Retorna una actividad de un integrante ingresado, junto con su index
+	// respectivo.
 	
 	public actividad getAmigoActividad(integrante amigo, int option) {
 		return amigo.getActividad(option-1);
 	}
+	
+	// Realiza el protocolo de iniciar una actividad en el proyecto:
+	// 
+	// Se establece la actividad ingresada como la actividadActual del proyecto, además
+	// de crear un nuevo registro de la actividad y convertirlo en el registroActual.
 	
 	public void iniciarActividad(integrante amigo, actividad act) {
 		this.ActividadActual = act;
@@ -51,12 +79,20 @@ public class controladorProyecto {
 		this.registroActual = registroNuevo;
 	}
 	
+	// Realiza el protocolo de finalizar una actividad en el proyecto:
+	//
+	// El registroActual se registra como terminado, se actualiza el tiempo de la actividadActual
+	// y se añade el registroActual al proyecto.
+	
 	public void finalizarTurno(integrante amigo) {
 		
 		int timeToAdd = registroActual.terminarTurno("Termine");
 		ActividadActual.actualizarTiempo(timeToAdd);
 		Proy.addLog(registroActual);
 	}
+	
+	// Añade a un usuario para que tenga el permiso de ingresar al proyecto desde
+	// su cuenta.
 	
 	public void addProyectsOfAmi(integrante amigo, String name, boolean isLider ) {
 		
@@ -66,21 +102,27 @@ public class controladorProyecto {
 		else {
 			amigo.addProyect(name, "false");
 		}
-		
 	}
+	
+	// Retorna el tiempo transcurrido del proyecto.
 	
 	public int getMinutes() {
 		return Proy.getTiempo() ;
 	}
 	
+	// Retorna el nombre del proyecto.
+	
 	public String getName() {
 		return Proy.getName() ;
 	}
 	
+	// Retorna la fecha en que se inició el proyecto.
 	
 	public String getStartTime() {
 		return Proy.getFechaInicial();
 	}
+	
+	// Retorna el integrante lider del proyecto.
 	
 	public integrante getLider()
 	{
@@ -88,6 +130,8 @@ public class controladorProyecto {
 		integrante amigo =  usuarios.get(liderName);
 		return amigo ;
 	}
+	
+	// Retorna un String con los proyectos de un integrante ingresado.
 	
 	public String getProyectsOfAmi(integrante amigo ) {
 		
@@ -98,9 +142,11 @@ public class controladorProyecto {
 		else {
 			temp =  temp.replace("[", "").replace("]", "");
 			return temp;
-		}
-		
+		}	
 	}
+	
+	// Retorna un String con un mensaje para imprimir en base a la información
+	// del proyecto actual.
 	
 	public String getProjectInfo() {
 		
@@ -109,14 +155,18 @@ public class controladorProyecto {
 		data += "Tiempo transcurrido desde el inicio: " + Proy.getTiempo() + "\n";
 		data += "Creador del proyecto: " + Proy.getLiderName() + "\n";
 		return data;
-		
 	}
+	
+	// Retorna un String con la lista de actividades pendientes de un integrante ingresado.
 	
 	public String getActividades(integrante Amigo)
 	
 	{
 		return Amigo.mostrarPendientes();
 	}
+	
+	// Agrega una actividad nueva a la lista de actividades por desarrollar de un estudiante
+	// cuyo nombre es ingresado y también el nombre de la actividad.
 	
 	public void agregarActividad(String aCargoDe, String name, String tipoActividad) 
 	
@@ -126,6 +176,10 @@ public class controladorProyecto {
 		usuarios.get(aCargoDe).setActivities(act);;
 	}
 	
+	// Realiza el protocolo de intentar iniciar sesión en el proyecto:
+	//
+	// Se consigue el integrante según el nombre ingresado y se compara su contraseña
+	// con la ingresada. Retorna el usuario según el nombre ingresado.
 	
 	public integrante iniciarSesion(String name, String pass) throws FileNotFoundException, IOException {
 		cargarUsuarios();
@@ -141,6 +195,8 @@ public class controladorProyecto {
 		
 	}
 	
+	// Retorna un boolean en base a si un integrante pertenece al proyecto actual.
+	
 	public boolean iniciarSesionProyecto(String name) {
 
 		integrante amigo= Proy.getIntegrante(name);
@@ -151,6 +207,8 @@ public class controladorProyecto {
 			return false;
 		}
 	}
+	
+	// Retorna un boolean en base a si el atributo Proy es igual a null o no.
 	
 	public boolean isNullProy() {
 		if (Proy == null) {
--- Entrega3/src/model/proyecto.java
+++ Entrega3/src/model/proyecto.java
@@ -4,6 +4,17 @@ import java.util.*;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 
+	// La clase "proyecto" posee los siguientes atributos:
+	// integrante Lider <<una instancia de la clase integrante que representa al lider del proyecto>>
+	// String Name <<un String con el nombre del proyecto>>
+	// HashMap<String, integrante> integrantes <<un HashMap con los integrantes del proyecto>>
+	// LocalDateTime fechaInicio <<una instancia de LocalDateTime con la fecha del inicio del proyecto>>
+	// int tiempoTranscurrido <<la cantidad de tiempo transcurrido en segundos (tipo entero)>>
+	// boolean Terminado <<boolean que define si el proyecto ha sido terminado>>
+	// HashMap<String, ArrayList<actividad>> actividades <<un HashMap con las actividades que faltan por acabar en el proyecto>>
+	// HashMap<String, ArrayList<actividad>> actividadesFinalizadas <<un HashMap con las actividades acabadas del proyecto>>
+	// registroActividad registros <<una instancia de la clase registroActividad con los registros del proyecto>>
+
 public class proyecto {
 	private integrante Lider;
 	private String Name;
@@ -15,6 +26,8 @@ public class proyecto {
 	private HashMap<String, ArrayList<actividad>> actividadesFinalizadas = new HashMap<String, ArrayList<actividad>>();
 	private registroActividad registros;
 	
+	// Crea un nuevo proyecto en base a un integrante (lider) 
+	// y el nombre del proyecto a crear.
 	
 	public proyecto(integrante Lider, String name) {
 		
@@ -28,9 +41,16 @@ public class proyecto {
 		
 	}
 	
+	// Añade un registro a los registros (clase registroActividad)
+	// dentro del proyecto.
+	
 	public void addLog(registro Log) {
 		registros.addLog(Log);
 	}
+	
+	// Cambia la fechaInicio a una ingresada
+	// a través de un String.
+	
 	public void putStartdate(String fecha) {
 		
 		DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss") ;
@@ -39,27 +59,43 @@ public class proyecto {
 		this.fechaInicio = inicio;
 	}
 	
+	// Cambia el tiempoTranscurrido a la cantidad (entera)
+	// ingresada.
+	
 	public void putCurrentTime(String seconds) {
 		this.tiempoTranscurrido = Integer.parseInt(seconds);
 	}
+	
+	// Devuelve el valor true/false de la variable
+	// Terminado (en boolean).
 	
 	public boolean estaTerminado() {
 		return Terminado;
 	}
 	
+	// Se añade una cantidad entera de segundos al tiempo
+	// transcurrido (tiempoTranscurrido).
+	
 	public void actualizarTiempo(int adicion) {
 		this.tiempoTranscurrido += adicion;
 	}
 	
+	// Los integrantes de un proyecto se pasan como un HashMap<String, integrante>
+	// y se convierten en la variable "integrantes" del proyecto.
+	
 	public void putIntegrantes(HashMap <String, integrante> integrantes) {
-		this.integrantes = integrantes ;
+		this.integrantes = integrantes;
 	}
+	
+	// Añade un integrante (amigo) a la lista de los integrantes del
+	// proyecto.
 	
 	public void agregarIntegrante(integrante amigo) {
 		integrantes.put(amigo.getName(), amigo);
-	
 	}
 	
+	// Se ingresa una actividad (act) a la lista de
+	// actividades por realizar del proyecto.
 	
 	public void agregarActividad(actividad act) {
 		String tipo = act.getTipoActividad();
@@ -67,39 +103,52 @@ public class proyecto {
 			actividades.put(tipo, new ArrayList<actividad>());
 		}
 		actividades.get(tipo).add(act);
-
 	}
+	
+	// Retorna el tiempo transcurrido durante la creación del proyecto.
 	
 	public int getTiempo() {
 		return tiempoTranscurrido;
 	}
 	
+	// Retorna la fecha en que se inició el proyecto en un String.
+	
 	public String getFechaInicial() {
-		return fechaInicio.toString();
-		
+		return fechaInicio.toString();	
 	}
+	
+	// Retorna el nombre del integrante lider del proyecto.
 	
 	public String getLiderName() {
 		String name = Lider.getName();
 		return name;
 	}
 	
+	// Ingresa el nombre de un integrante del proyecto y retorna su respectivo
+	// integrante.
+	
 	public integrante getIntegrante(String name) {
 		integrante amigo = integrantes.get(name);
 		return amigo;
-		
 	}
+	
+	// Retorna el nombre y el correo de un integrante del proyecto en base al
+	// nombre de este mismo.
 	
 	public String getIntegrantData(String name) {
 		integrante amigo = integrantes.get(name);
 		String data = "Nombre: " + amigo.getName() + "Correo: " + amigo.getCorreo(); 
 		return data;
-		
 	}
+	
+	// Retorna el nombre del proyecto.
 	
 	public String getName() {
 		return Name;
 	}
+	
+	// Calcula en base a dos fechas (inicial y final) la cantidad de tiempo que
+	// ha transcurrido entre estas dos.
 	
 	private void calcularTiempoDesdeInicio(){
 		LocalDateTime fechaActual= LocalDateTime.now();
@@ -108,6 +157,9 @@ public class proyecto {
 		int hours = (finalTime - inicio)/3600; 
 		this.tiempoTranscurrido = hours;
 	}
+	
+	// Realiza el procedimiento para finalizar una actividad, el cual es añadir
+	// la actividad que se quiere finalizar en la lista de actividades finalizadas.
 	
 	public void finalizarActividad(actividad act) {
 
@@ -119,6 +171,10 @@ public class proyecto {
 
 
 	}
+	
+	// Realiza el procedimiento para finalizar un proyecto, el cual es cambiar su valor de Terminado
+	// a true, se crea una actividad llamada "Proyecto finalizado", se crea un registro en base a esta
+	// actividad y se añade el registro a los registros del proyecto.
 	
 	public void finalizarProyecto(String comentario) {
 		this.Terminado = true;
